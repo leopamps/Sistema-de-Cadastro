6482eff5979080a8ae8e2c7f464ff9ed5715caa0
leopamps <leo.pamplona96@hotmail.com>
2024-07-05 21:09:54 -0300

    att

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
 const Sequelize = require('sequelize')
@@ -7,7 +7,7 @@ const port = 3000
 
 //config
     //template engine
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
     app.set('view engine','handlebars')
     //conexao com o banco de dados sql
     const sequelize = new Sequelize('teste','root', 'admin', {
@@ -16,11 +16,10 @@ const port = 3000
     })
 
 //Rotas
-    app.get('/cad', function(req, res){
-        res.send("Rota de cadastro")
+    app.get('/cadastro', function(req, res){
+       res.render('formulario')
     })
 
-    
 
 //escuta a porta 3000
 app.listen(port, function(){
